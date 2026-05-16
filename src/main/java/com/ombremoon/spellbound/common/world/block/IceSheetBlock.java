87de0ee77fc6bef6b51af11c1500beaba7075df5
OmbreMoon <brycemcnair600@gmail.com>
2026-05-16 01:50:26 -0700

    ice skate 80% complete

--- src/main/java/com/ombremoon/spellbound/common/world/block/IceSheetBlock.java
+++ src/main/java/com/ombremoon/spellbound/common/world/block/IceSheetBlock.java
@@ -1,12 +1,16 @@
 package com.ombremoon.spellbound.common.world.block;
 
 import com.mojang.serialization.MapCodec;
+import com.ombremoon.spellbound.common.init.SBSkills;
 import com.ombremoon.spellbound.common.world.block.entity.IceSheetBlockEntity;
 import com.ombremoon.spellbound.main.Constants;
+import com.ombremoon.spellbound.util.SpellUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.*;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -36,7 +40,51 @@ public class IceSheetBlock extends BaseEntityBlock {
 
     @Override
     protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
-        level.removeBlock(pos, false);
+        BlockEntity blockEntity = level.getBlockEntity(pos);
+        if (blockEntity instanceof IceSheetBlockEntity iceSheetBlockEntity && iceSheetBlockEntity.getOwner() != null) {
+            level.removeBlock(pos, false);
+        } else {
+            level.removeBlockEntity(pos);
+        }
+    }
+
+    @Override
+    protected void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
+        BlockEntity entity = level.getBlockEntity(pos);
+
+
+        super.onRemove(state, level, pos, newState, movedByPiston);
+    }
+
+    @Override
+    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+        super.stepOn(level, pos, state, entity);
+        BlockEntity blockEntity = level.getBlockEntity(pos);
+        if (blockEntity instanceof IceSheetBlockEntity iceSheetBlockEntity) {
+            iceSheetBlockEntity.stepOn(level, pos, state, entity);
+        } else {
+            entity.setIsInPowderSnow(true);
+        }
+    }
+
+    @Override
+    public float getFriction(BlockState state, LevelReader levelReader, BlockPos pos, @Nullable Entity entity) {
+        if (entity == null)
+            return super.getFriction(state, levelReader, pos, entity);
+
+        Level level = entity.level();
+        BlockEntity blockEntity = level.getBlockEntity(pos);
+        if (blockEntity instanceof IceSheetBlockEntity iceSheetBlockEntity && iceSheetBlockEntity.getOwner() != null) {
+            Player player = level.getPlayerByUUID(iceSheetBlockEntity.getOwner());
+            if (player != null) {
+                var skills = SpellUtil.getSkills(player);
+                if (entity.is(player) || skills.hasSkill(SBSkills.SNOW_BOOTS) && SpellUtil.IS_ALLIED.test(entity, player)) {
+                    return 0.6F;
+                }
+            }
+        }
+
+        return super.getFriction(state, levelReader, pos, entity);
     }
 
     @Override
